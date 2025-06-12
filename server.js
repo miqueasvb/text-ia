2e6329f9308577f0b60804ef09227f689426d34c
miqueasvico <90068766+miqueasvb@users.noreply.github.com>
2025-06-12 12:45:19 -0300

    server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-const express = require('express');
-const cors = require('cors');
-const { Configuration, OpenAIApi } = require('openai');
-require('dotenv').config();
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { Configuration, OpenAIApi } from 'openai';
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
